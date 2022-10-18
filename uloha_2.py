554f74b2c963e513ee6025bd1ce3be1b05f9f3d5
Weragin <109145593+Weragin@users.noreply.github.com>
2022-10-18 21:52:30 +0200

    Delete uloha_2.py

--- uloha_2.py
+++
@@ -1,9 +0,0 @@
-def uloha_2(num, a, b):
-
-    return a <= num <= b
-
-
-def uloha_2(num, ARR):  #overloadol som to, aby to prijímalo aj arraye a tuple s dĺžkou aspoň 2
-
-    return ARR[0] <= num <= ARR[1]
-
