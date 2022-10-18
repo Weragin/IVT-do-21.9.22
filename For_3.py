654007ea127b06238f96bce2d54ac63eab9c645c
Weragin <109145593+Weragin@users.noreply.github.com>
2022-10-18 21:51:07 +0200

    Delete For_3.py

--- For_3.py
+++
@@ -1,9 +0,0 @@
-def div4_7(N):
-	tot = 0
-	
-	for i in range(N):
-		tot += (N%4 == 0 and N%7 == 0)
-	print(tot)
-	# rychlejsie by bolo vyprintovat floor(N/28)
-
-print(div4_7(int(input("Zadajte N: "))))
